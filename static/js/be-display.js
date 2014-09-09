eac09261d178000944746dcc089921717ea22648
lenin.anduin <leninsuro@gmail.com>
2014-09-09 14:51:18 -0500

    theme update & local storage implementation

--- static/js/be-display.js
+++ static/js/be-display.js
@@ -1,5 +1,56 @@
-if (render == 'projects_list'){
-	var b_data, html_template;
+var debug = 1;
+function _log(s){
+	if (debug){
+		console.log(s);
+	}
+	return false;
+}
+
+//check the support of local storage
+local_storage = 0;
+if(typeof(Storage) !== "undefined") {
+    local_storage = 1;
+}
+
+function renderProjects(_data){
+	if (_data.projects.length > 0 ){
+        for (var i in _data.projects) {
+            var owners = new Array();
+
+            p = _data.projects[i];
+            html_template = $(tpl).eq(0).clone();
+
+            var params = {id:p.id , name:p.name };
+            var url = 'project.html?'+$.param(params);
+
+            $("a.project_link", html_template).attr("href", url);
+            //cover
+            $("img.cover", html_template).attr("src", p.covers[404]);
+            //name
+            $(".name", html_template).html(p.name);
+            //owners
+            for (var o in p.owners){
+                owners[o] = "<a href='"+p.owners[o].url+"' class='accent-txt'>" + p.owners[o].display_name +"</a>";
+            }
+            $(".owners", html_template).html(owners.join(', '));
+            //tags - fields
+            $(".fields", html_template).html(p.fields.join(', '));
+            //stats
+            $(".projects").append(html_template);
+            //stats
+            for (var o in p.stats){
+                var stat = "<span class='"+o+"'><i class='accent-txt'></i>" + p.stats[o] + "&nbsp;" + o +"</span>";
+                $(".stats", html_template).prepend(stat);
+            }
+        }
+        $(tpl).eq(0).remove();
+    }
+}
+
+var render = location.pathname.match(/(\w+\.\w+)/g);
+
+if (render == 'index.html'){
+	var _data, html_template;
 	var tpl = '.project';
 
 	$(function() {
@@ -9,46 +60,31 @@ if (render == 'projects_list'){
 		}if ( !user_id  ){
 
 		}else{
-			be.user.projects(user_id, {}, function(d){
-			    b_data = d;
-			    if (b_data.projects.length > 0 ){
-			        for (var i in b_data.projects) {
-			            var owners = new Array();
+			if (local_storage){
+				if ( !localStorage.user_projects ){
+					fetchUserProjects();
+				}else{
+					//caceh time 1hr
+					passed_hrs = Math.floor( (Date.now() - localStorage.user_projects_updated) / 1000 / 60 / 60);
+					if ( passed_hrs > 1 ){
+						//updating local data
+						fetchUserProjects();
+					}else{
+						//using local data
+						renderProjects(JSON.parse( localStorage.user_projects ));
+					}
+				}
 
-			            p = b_data.projects[i];
-			            html_template = $(tpl).eq(0).clone();
-
-			            var params = {id:p.id , name:p.name };
-			            var url = 'project.html?'+$.param(params);
-
-			            $("a.project_link", html_template).attr("href", url);
-			            //cover
-			            $("img.cover", html_template).attr("src", p.covers[404]);
-			            //name
-			            $(".name", html_template).html(p.name);
-			            //owners
-			            for (var o in p.owners){
-			                owners[o] = "<a href='"+p.owners[o].url+"' class='accent-txt'>" + p.owners[o].display_name +"</a>";
-			            }
-			            $(".owners", html_template).html(owners.join(', '));
-			            //tags - fields
-			            $(".fields", html_template).html(p.fields.join(', '));
-			            //stats
-			            $(".projects").append(html_template);
-			            //stats
-			            for (var o in p.stats){
-			                var stat = "<span class='"+o+"'><i class='accent-txt'></i>" + p.stats[o] + "&nbsp;" + o +"</span>";
-			                $(".stats", html_template).prepend(stat);
-			            }
-			        }
-			        $(tpl).eq(0).remove();
-			    }
-			});
+			}else{
+				be.user(user_id, function(d){
+					renderProjects(d);
+				});
+			}
 		}
 	});
 }
 
-if (render == 'project'){
+if (render == 'project.html'){
 	var b_data, html_template;
 	var tpl = '.project_c';
 	var p_id = getUrlVar('id');
@@ -103,29 +139,36 @@ if (render == 'project'){
 		                var stat = "<span class='"+o+"'><i class='accent-txt'></i>" + p.stats[o] + "&nbsp;" + o + "</span>";
 		                $(".stats", html_template).append(stat);
 		            }
+		            //behance link
+		            $('a.gotobehance').attr('href', p.url);
+
 		            //comments
 		            be.project.comments(p_id, {},  function(d){
-		            	for (var i in d.comments ){
-		            		var comment = d.comments[i];
+		            	if( d.comments.length > 0 ){
+			            	for (var i in d.comments ){
+			            		var comment = d.comments[i];
 
-		            		var comment_template = $('.comments_c li',html_template ).eq(0).clone();
-		            		//user avatar
-		            		$("img",comment_template).attr('src', comment.user.images[50] );
-		            		//user name
-		            		$('.user_name', comment_template).text( comment.user.display_name );
-		            		//date on
-		            		var date_on = new Date(comment.created_on*1000);
-		            		var date_str = date_on.toUTCString('dddd MMM yyyy h:mm').substring(0, 22);
-		            		console.log();
-							$('.date_on', comment_template).text( date_str );
+			            		var comment_template = $('.comments_c li',html_template ).eq(0).clone();
+			            		//user avatar
+			            		$("img",comment_template).attr('src', comment.user.images[50] );
+			            		//user name
+			            		$('.user_name', comment_template).text( comment.user.display_name );
+			            		//date on
+			            		var date_on = new Date(comment.created_on*1000);
+			            		var date_str = date_on.toUTCString('dddd MMM yyyy h:mm').substring(0, 22);
+			            		console.log();
+								$('.date_on', comment_template).text( date_str );
 
-		            		//comment
-		            		$('.comment', comment_template).text( comment.comment );
+			            		//comment
+			            		$('.comment', comment_template).text( comment.comment );
 
-		            		$('.comments_c',html_template).append(comment_template);
-		            	}
-		            	$('.comments_c li',html_template ).eq(0).remove();
-		            });
+			            		$('.comments_c',html_template).append(comment_template);
+			            	}
+			            	$('.comments_c li',html_template ).eq(0).remove();
+			            }else{
+			            	$('.comments-box').hide();
+			            }
+			        });
 
 			    }
 			});
@@ -133,7 +176,103 @@ if (render == 'project'){
 	});
 }
 
+function renderSocialLink(elem){
+	if ( $('nav').length > 0 ){
+		var new_link = $('<a href="" target="_blank" class="accent-txt"></a>');
+		$(new_link).attr('href', elem.url).html(elem.service_name)
+		if ( elem.url.match( /(mailto:)/g ) ){
+			$(new_link).removeAttr('target');
+		}
+		$('nav').append($(new_link));
+	}
+	return false;
+}
+
+function renderUser(_user_data){
+	if ( _user_data.http_code == 200 ){
+
+		//avatar image
+		if ( $('div.avatar').length > 0 ){
+			$('div.avatar > img').attr('src', _user_data.user.images[115]);
+		}
+		//username
+		if ( $('.username > a').length > 0  ){
+			$('.username > a').html(_user_data.user.display_name);
+		}
+		$('title').text(_user_data.user.display_name);
+
+		//userdescription
+		if ( $('.userdescription').length > 0  ){
+			$('.userdescription').html(_user_data.user.occupation);
+		}
+		//social links
+		for( var i in _user_data.user.social_links ){
+			renderSocialLink( _user_data.user.social_links[i] );
+		}
+		//extra links
+		for ( var i in extra_links ){
+			if ( !extra_links[i].url.match( /(\/\/)/g ) && ! extra_links[i].url.match( /(mailto:)/g ) ){
+				extra_links[i].url = '//'+extra_links[i].url;
+			}
+			renderSocialLink(extra_links[i]);
+		}
+	}
+	return false;
+}
+
+function fetchUserData(){
+	be.user(user_id, function(d){
+		localStorage.setItem("user_data", JSON.stringify(d) );
+		localStorage.setItem("user_data_updated", Date.now() );
+		renderUser(d);
+	});
+	return false;
+}
+
+function fetchUserProjects(){
+	be.user.projects(user_id, {}, function(d){
+	    b_data = d;
+	    localStorage.setItem("user_projects", JSON.stringify(d) );
+		localStorage.setItem("user_projects_updated", Date.now() );
+		renderProjects(b_data);
+	});
+
+	return false;
+}
+
+function renderUserCall(){
+	if (user_id){
+		if (local_storage){
+			if ( !localStorage.user_data ){
+				fetchUserData();
+			}else{
+				//caceh time 1hr
+				passed_hrs = Math.floor( (Date.now() - localStorage.user_data_updated) / 1000 / 60 / 60);
+				if ( passed_hrs > 1 ){
+					//updating local data
+					fetchUserData();
+				}else{
+					//using local data
+					renderUser(JSON.parse( localStorage.user_data ));
+				}
+			}
+
+		}else{
+			be.user(user_id, function(d){
+				renderUser(d);
+			});
+		}
+
+	}
+}
+
+
+$(function(){
+	renderUserCall();
+});
+
 function getUrlVar(key){
 	var result = new RegExp(key + "=([^&]*)", "i").exec(window.location.search);
 	return result && unescape(result[1]) || "";
 }
+
--- static/js/be-settings.js
+++ static/js/be-settings.js
@@ -1,3 +1,16 @@
 //we need this awesome vars
 var user_id = "batebate"; //your username
 var behance_api_key = "8WLknnl5NIKSyTzYZw9kX1soGwPZ6duT"; //DO NOT CHANGE
+
+//extra links
+//you can add as many as you need
+var extra_links = [
+    {
+        "service_name": "Facebook",//link text
+        "url": "facebook.com" //link url
+    },
+    {
+        "service_name": "Contact me",
+        "url": "mailto:holabate@gmail.com"
+    }
+]
